a7ed998d6fcac02d36555a8dbede55e192eb6722
michaelsatumba <mjsatumba@gmail.com>
2022-04-26 13:43:18 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert('hello');
+
+let p = document.getElementById('p1');
+
+// p.innerHTML = 'change';
+
+const array1 = [1, 25, 3];
+
+function add1() {
+	x + 1;
+}
+
+const length1 = array1.length;
+
+let i = 0;
+
+while (i < array1.length) {
+	x = array1[i];
+	x = x + 1;
+	i++;
+	console.log(x);
+}
+
+p.innerHTML = array1[1];
